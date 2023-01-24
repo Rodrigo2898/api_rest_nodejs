2efb1a89d1eb1f000b60065e8138a5c03e56eb33
Rodrigo <rodrigofeitosarodrigues@gmail.com>
2023-01-24 19:57:37 -0300

    Projeto Criado e primeiras configurações

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'class-methods-use-this': 'off',
+  },
+  'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+
+};
