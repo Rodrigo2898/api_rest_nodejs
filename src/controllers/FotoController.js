1c36a7d7a061377ad36c0ae7b2dd23e115b0caa1
Rodrigo <rodrigofeitosarodrigues@gmail.com>
2023-02-21 21:08:03 -0300

    Salvando fotos na base de dados

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,19 +1,25 @@
 import multer from "multer";
 import multerConfig from "../config/multerConfig";
 
+import Foto from '../models/Foto';
+
 const upload = multer(multerConfig).single('foto');
 
 
 class FotoController {
-  async store(req, res) {
-    return upload(req, res, (error) => {
+   store(req, res) {
+    return upload(req, res, async (error) => {
       if(error) {
         return res.status(400).json({
           errors: [error.code],
         });
       }
 
-      res.json(req.file);
+      const { originalname, filename } = req.file;
+      const { aluno_id } = req.body;
+      const foto = await Foto.create({ originalname, filename, aluno_id });
+
+      res.json(foto);
     });
   }
 }
--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import fotoController from "../controllers/FotoController";
+import loginRequired from "../middlewares/loginRequired";
 
 const router = new Router();
 
-router.post('/', fotoController.store);
+router.post('/', loginRequired,fotoController.store);
 
 export default router;
